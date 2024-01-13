93568070d3f1c26917b4f26128ac8bb5472a0d99
Becky449 <rabnab12@gmail.com>
2024-01-13 12:48:41 +0300

    Set up revocation strategy

--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,16 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
-end
+
   resources :users
+
   namespace :api, defaults: { format: 'json' } do
     resources :schools, only: [:index, :show]
     resources :students, only: [:index, :show, :create, :update, :destroy]
     resources :messages, only: [:index, :show, :create, :update, :destroy]
     resources :metrics, only: [:index, :show, :create, :update, :destroy]
   end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
